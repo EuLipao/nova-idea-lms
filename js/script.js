58d9fd227f0b62685fbf8171f5e92f6896fc08ba
EuLipao <70975952+EuLipao@users.noreply.github.com>
2022-07-20 22:35:14 -0300

    adicionado outras funções

--- js/script.js
+++ js/script.js
@@ -5,7 +5,13 @@ if(userId.length <= 0){
     window.alert('Digite um usuário válido!')
 }else if( userId == 'lfernandes'){
     window.location.href="auxlog/auxlog1.html"
-}else{
+}else if( userId == 'auxlog01'){
+    window.location.href="auxlog/auxlog1.html"
+}else if( userId == 'operador01'){
+    window.location.href="operador/operadorEmpilhadeira.html"
+}else if( userId == 'auxlog02'){
+    window.location.href="auxlog2/auxlog2.html"}
+else{
     window.alert('Digite um usuário valido!')
 }
 
